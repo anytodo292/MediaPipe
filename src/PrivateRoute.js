3817a59ba1d642e6eae516df0a0eba2b368d576d
anytodo92 <anything.todo292@gmail.com>
2024-05-27 04:13:55 +0200

    first commit

---
+++ src/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  // return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+  return <Outlet />;
+};
+
+export default PrivateRoute;
